3b262b116002ae75681028b2a6b8c056ececf080
Matt Gumbley <matt.gumbley@devzendo.org>
2016-06-02 08:41:55 +0100

    Initial commit of output of: module-starter --module=Ham::WSJTX::Logparse --author="Matt Gumbley" --email=matt.gumbley@devzendo.org --license=apache --ignores=hg

---
+++ Ham-WSJTX-Logparse/lib/Ham/WSJTX/Logparse.pm
@@ -0,0 +1,117 @@
+package Ham::WSJTX::Logparse;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Ham::WSJTX::Logparse - The great new Ham::WSJTX::Logparse!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Ham::WSJTX::Logparse;
+
+    my $foo = Ham::WSJTX::Logparse->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Matt Gumbley, C<< <matt.gumbley at devzendo.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-ham-wsjtx-logparse at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Ham-WSJTX-Logparse>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Ham::WSJTX::Logparse
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Ham-WSJTX-Logparse>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Ham-WSJTX-Logparse>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Ham-WSJTX-Logparse>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Ham-WSJTX-Logparse/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2016 Matt Gumbley.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    L<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+=cut
+
+1; # End of Ham::WSJTX::Logparse
